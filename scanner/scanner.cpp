5e7d0a714bbd45158a24e641af9eb32bb18c9430
MoayadR <moayad.r.abdalraouf@gmail.com>
2023-10-27 01:34:20 +0200

    + FileCOntent Attribute to Scanner

--- scanner/scanner.cpp
+++ scanner/scanner.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include "scanner.h"
+#include <sstream>
 
 using namespace std;
 
-Scanner::Scanner() {}
+Scanner::Scanner() {
+    this->generateReserverWords();
+}
 
 Scanner::Scanner(const string &fileName) {
-    this->fileHandler.open(fileName);
-    this->position = 0;
+    this->setFileContent(fileName);
+    this->generateReserverWords();
     this->generateListOfTokens();
 }
 
 void Scanner::generateListOfTokens() {
     while (true)
     {
-        Token temp = this->getTokenOnWhiteSpace();
+        Token temp = this->getToken();
         if (temp.getType() == EndOfFile) // stopping Condition;
         {
             break;
@@ -23,29 +26,39 @@ void Scanner::generateListOfTokens() {
     }
 }
 
-Token Scanner::getToken() {
-
-}
-
-Token Scanner::getTokenOnWhiteSpace() {
+Token Scanner::getToken() { // Depend on FileContent
     string temp = "";
     char c;
-    while(this->fileHandler >> noskipws >> c)
+    while(fileHandler >> noskipws >> c)
     {
+        if (c == '{') // handling comments in TinyC
+        {
+            while(fileHandler>>noskipws>>c)
+            {
+                if (c== '}')
+                {
+                    fileHandler>>noskipws>>c;
+                    break;
+                }
+            }
+        }
+
         if (c == ' ' && temp.length() == 0)
             continue;
         else if (c == ' ')
             return Token(temp , this->getTokenType(temp));
 
+        if(this->lookAheadForDelimiters() && temp.length())
+            return Token(temp , this->getTokenType(temp));
+
         temp += c;
     }
-
     if (temp.length())
         return Token(temp , this->getTokenType(temp));
 
-    this->fileHandler.close();
     return Token("" , EndOfFile);
 }
+
 
 tokenType Scanner::getTokenType(const string value) { // detect the type of the token
     return InvalidToken;
@@ -58,6 +71,46 @@ void Scanner::printListOfTokens() {
     }
 }
 
+void Scanner::generateReserverWords() {
+    this->delimiters["read"] = InputToken;
+    this->delimiters["write"] = OutputToken;
+    this->delimiters[";"] = SemiColumnToken;
+    this->delimiters["if"] = IFToken;
+    this->delimiters["then"] = ThenToken;
+    this->delimiters["else"] = ElseToken;
+    this->delimiters["end"] = EndToken;
+    this->delimiters["repeat"] = RepeatToken;
+    this->delimiters["until"] = UntilToken;
+    this->delimiters["*"] = MULToken;
+    this->delimiters["/"] = DIVToken;
+    this->delimiters["+"] = PlusToken;
+    this->delimiters["-"] = SubToken;
+    this->delimiters[":="] = AssignToken;
+    this->delimiters["<"] = LTToken;
+    this->delimiters["="] = ETToken;
+    this->delimiters[">"] = GTToken;
+    this->delimiters["("] = OpenningBracketsToken;
+    this->delimiters[")"] = ClosingBracketsToken;
+    this->delimiters["{"] = OpenningCurlyBracketsToken;
+    this->delimiters["}"] = ClosingCurlyBracketsToken;
+}
+
+bool Scanner::lookAheadForDelimiters() {
+    string temp = "";
+    char c ;
+    for(int i =0 ; i<6 ; i++)
+    {
+
+    }
+}
+
+void Scanner::setFileContent(const string &fileName) {
+    fstream fileHandler(fileName);
+    std::stringstream buffer;
+    buffer << fileHandler.rdbuf();
+    this->fileContent =  buffer.str();
+    fileHandler.close();
+}
 
 
 Token::Token(const string &value, tokenType type) : value(value), type(type) {}
--- scanner/scanner.h
+++ scanner/scanner.h
@@ -4,14 +4,28 @@
 
 #ifndef SCANNER_SCANNER_H
 #define SCANNER_SCANNER_H
+#define LOOKAHEAD_BUFFER 6
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 typedef enum tokenType {
- EndOfFile , InvalidToken
-}tokenType;
+ EndOfFile ,
+ InvalidToken ,
+ InputToken , OutputToken ,
+ SemiColumnToken , IFToken ,
+ ThenToken , ElseToken ,
+ EndToken , RepeatToken ,
+ UntilToken , MULToken ,
+ DIVToken , PlusToken ,
+ SubToken , AssignToken ,
+ LTToken , ETToken ,
+ GTToken , OpenningBracketsToken,
+ ClosingBracketsToken , OpenningCurlyBracketsToken,
+ ClosingCurlyBracketsToken
+} tokenType;
 
 class Token{
 private:
@@ -34,18 +48,21 @@ public:
 
 class Scanner{
 private:
-    fstream fileHandler;
     vector<Token>listOfTokens;
-    int position ; // for error handling
+    unordered_map<string, tokenType>delimiters;
+    string fileContent;
+    int position; // error handling
 
 public:
     Scanner();
     Scanner(const string& fileName);
     Token getToken();
-    Token getTokenOnWhiteSpace();
     void generateListOfTokens();
     tokenType getTokenType(const string value);
     void printListOfTokens();
+    void generateReserverWords();
+    void setFileContent(const string& fileName);
+    bool lookAheadForDelimiters();
 };
 
 
